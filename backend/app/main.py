f90fd8f33374f5cf34733761fe0c9e43b7c96eef
manish-max07 <111220535+manish-max07@users.noreply.github.com>
2026-07-08 09:36:00 +0530

    Allowed origin added frontend deployed link

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Manish Kumar Portfolio API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://manish-kumar-portfolio-omega.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
